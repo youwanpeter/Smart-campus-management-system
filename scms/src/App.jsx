3ea004d1894fa21f5d8d99d8654c1b79f4e08570
youwanpeter <youwanpeter226@gmail.com>
2025-02-27 01:23:01 +0530

    dashboard first look done

--- scms/src/App.jsx
+++ scms/src/App.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-import { Layout } from "antd";
-import Sidebar from "./components/Sidebar";
 import { useState } from "react";
+import React from "react";
+import { Button, Layout } from "antd";
+import Sidebar from "./components/Sidebar";
+import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+import CustomHeader from "./components/Header";
 import "./App.css";
 
 const { Sider, Header, Content } = Layout;
@@ -17,9 +19,18 @@ const App = () => {
         className="sider"
       >
         <Sidebar />
+
+        <Button
+          type="text"
+          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          onClick={() => setcollapsed(!collapsed)}
+          className="triger-btn"
+        />
       </Sider>
       <Layout>
-        <Header className="header"></Header>
+        <Header className="header">
+          <CustomHeader />
+        </Header>
         <Content className="content"></Content>
       </Layout>
     </Layout>
--- scms/src/components/Sidebar.jsx
+++ scms/src/components/Sidebar.jsx
@@ -1,6 +1,17 @@
 import { Flex, Menu } from "antd";
 import React from "react";
 import { FaLeaf } from "react-icons/fa6";
+import {
+  UserOutlined,
+  FileTextOutlined,
+  LogoutOutlined,
+  SettingOutlined,
+  DashboardOutlined,
+  ScheduleOutlined,
+  NotificationOutlined,
+  BookOutlined,
+  WechatOutlined,
+} from "@ant-design/icons";
 
 const Sidebar = () => {
   return (
@@ -11,7 +22,58 @@ const Sidebar = () => {
         </div>
       </Flex>
 
-      <Menu></Menu>
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={["1"]}
+        className="menu-bar"
+        items={[
+          {
+            key: "1",
+            icon: <DashboardOutlined />,
+            label: "Dashboard",
+          },
+          {
+            key: "2",
+            icon: <UserOutlined />,
+            label: "Users",
+          },
+          {
+            key: "3",
+            icon: <ScheduleOutlined />,
+            label: "Schedule",
+          },
+          {
+            key: "4",
+            icon: <NotificationOutlined />,
+            label: "Events",
+          },
+          {
+            key: "5",
+            icon: <BookOutlined />,
+            label: "Resource",
+          },
+          {
+            key: "6",
+            icon: <WechatOutlined />,
+            label: "Communication ",
+          },
+          {
+            key: "7",
+            icon: <FileTextOutlined />,
+            label: "Reports ",
+          },
+          {
+            key: "8",
+            icon: <SettingOutlined />,
+            label: "Settings",
+          },
+          {
+            key: "9",
+            icon: <LogoutOutlined />,
+            label: "LogOut",
+          },
+        ]}
+      />
     </>
   );
 };
