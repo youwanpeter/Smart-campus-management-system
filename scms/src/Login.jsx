c987a811d694cc3db7560c75d751860bcac71283
youwanpeter <youwanpeter226@gmail.com>
2025-03-04 15:56:04 +0530

    youwan add some changes in login css

--- scms/src/Login.jsx
+++ scms/src/Login.jsx
@@ -3,30 +3,30 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "antd";
 import "./Login.css";
 import LoginForm from "./pages/components/Logincontent/LoginForm";
-import { FaLeaf } from "react-icons/fa6"; 
+import { FaLeaf } from "react-icons/fa6";
 
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <section className="LoginForm">
-            {/* Gradient BG */}
-            <div className="animated-background"></div>
+  return (
+    <section className="LoginForm">
+      {/* Gradient BG */}
+      <div className="animated-background"></div>
 
-            {/* Content */}
-            <div className="form-content">
-                <FaLeaf className="logo-icon" />
-                <h2>Login/Sign Up</h2>
-                <p>Welcome! Please enter your credentials below.</p>
+      {/* Content */}
+      <div className="form-content">
+        <FaLeaf className="logo-icon" />
+        <h2>Login/Sign Up</h2>
+        <p>Welcome! Please enter your credentials below.</p>
 
-                <LoginForm />
+        <LoginForm />
 
-                <Button type="primary" onClick={() => navigate("/app")}>
-                    Login
-                </Button>
-            </div>
-        </section>
-    );
+        <Button type="primary" onClick={() => navigate("/")}>
+          Login
+        </Button>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
