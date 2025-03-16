732b84feadd6170c5e559ad8ba7524074a7929d7
AmandaKarunanayaka <136224022+AmandaKarunanayaka@users.noreply.github.com>
2025-03-16 23:44:03 +0530

    index

--- server/index.js
+++ server/index.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-require("dotenv").config();
-const express = require("express");
-const connectDB = require("./db");
-const LoginDetailsModel = require("./models/Login_details.js"); // Fixed typo
-const EventModel = require("./models/Event.js");
-const eventRoutes = require("./routes/eventRoutes");
-const cors = require("cors");
-
-const app = express();
-app.use(express.json());
-
-// Configure CORS to allow multiple origins (localhost:3000 and localhost:5173)
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"], // Add all frontend origins that need access
-    methods: "GET, POST, PUT, DELETE", // Allowed HTTP methods
-    allowedHeaders: "Content-Type, Authorization", // Allowed headers
-  })
-);
-
-// Connect to MongoDB
-connectDB();
-
-// Use the event routes for events-related API
-app.use("/api/events", eventRoutes);
-
-// Get the port from environment variables or use default 5000
-=======
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./db');
@@ -55,7 +26,6 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
->>>>>>> ddbcf091640c70d7061dc96ded5b7d6d77f9deb6
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
